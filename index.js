613f8c172995e53928a26d86f7cfefcd902b32f8
prat-git <poornimagiri22@gmail.com>
2025-05-12 12:54:07 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Variables in JavaScript//
+let AccountName= "Purnima"
+var AccountPassword= "1234"
+const AccountId= 2082
+
+console.log(AccountId);
+console.log(AccountName);
+console.log(AccountPassword);
