f7f100a4254120819422423d534dd18343a79cc9
prat-git <poornimagiri22@gmail.com>
2025-05-13 14:07:34 +0545

    Add new

--- index.js
+++
@@ -1,8 +0,0 @@
-// Variables in JavaScript//
-let AccountName= "Purnima"
-var AccountPassword= "1234"
-const AccountId= 2082
-
-console.log(AccountId);
-console.log(AccountName);
-console.log(AccountPassword);
